ba9938d0f2cea6f78eab541de7d2adb393d41164
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-12-12 01:19:06 +0100

    Fix scripts

--- cli/src/script/scala-native-c.bat
+++ cli/src/script/scala-native-c.bat
@@ -1,18 +1,21 @@
 @ECHO OFF
+SetLocal
+
 set SCALA_BIN_VER=@SCALA_BIN_VER@
 set SCALANATIVE_VER=@SCALANATIVE_VER@
-
+set SCALANATIVE_BIN_VER=@SCALANATIVE_BIN_VER@
+echo %*
 for /F "tokens=5" %%i in (' scala -version 2^>^&1 1^>nul ') do set SCALA_VER=%%i
 
 if NOT "%SCALA_VER:~0,4%" == "%SCALA_BIN_VER%" (
   echo "This bundle of Scala Native CLI is for %SCALA_BIN_VER%. Your scala version is %SCALA_VER%!" 1>&2
-) else (
-  set PLUGIN=%~dp0\..\lib\nscplugin_%SCALA_VER%-%SCALANATIVE_VER%.jar
-  set CLASSPATH="."
-  set libs=nativelib clib posixlib windowslib auxlib javalib scalalib
-  (for %%lib in (%libs%) do ( 
-    set CLASSPATH="%CLASSPATH%:%~dp0\..\lib\%%lib_%SCALA_BIN_VER%-%SCALANATIVE_VER%.jar"
-  ))
-
-  scalac -classpath "%CLASSPATH%" "-Xplugin:%PLUGIN%" %*
+  EXIT 1
 )
+
+set BASE=%~dp0\..\lib
+set SUFFIX=_native%SCALANATIVE_BIN_VER%_%SCALA_BIN_VER%-%SCALANATIVE_VER%.jar
+
+set PLUGIN=%BASE%\nscplugin_%SCALA_VER%-%SCALANATIVE_VER%.jar
+set NATIVELIB=%BASE%\nativelib%SUFFIX%;%BASE%\clib%SUFFIX%;%BASE%\posixlib%SUFFIX%;%BASE%\windowslib%SUFFIX%;%BASE%\auxlib%SUFFIX%;%BASE%\javalib%SUFFIX%;%BASE%\scalalib%SUFFIX%
+  
+scalac -classpath .;%NATIVELIB% -Xplugin:%PLUGIN% "%*"
--- cli/src/script/scala-native-ld.bat
+++ cli/src/script/scala-native-ld.bat
@@ -1,13 +1,15 @@
 @ECHO OFF
+SetLocal 
+
 set SCALA_BIN_VER=@SCALA_BIN_VER@
 set SCALANATIVE_VER=@SCALANATIVE_VER@
 set SCALANATIVE_BIN_VER=@SCALANATIVE_BIN_VER@
 
-set CLILIB="%~dp0\..\lib\scala-native-cli-assembly_%SCALA_BIN_VER%-%SCALANATIVE_VER%.jar"
-set NATIVELIB=""
-set libs=nativelib clib posixlib windowslib auxlib javalib scalalib
-(for %%lib in (%libs%) do ( 
-   set NATIVELIB="%NATIVELIB% %~dp0\..\lib\%%lib_%SCALA_BIN_VER%-%SCALANATIVE_VER%.jar"
-))
+set BASE=%~dp0\..\lib
+set SUFFIX=_native%SCALANATIVE_BIN_VER%_%SCALA_BIN_VER%-%SCALANATIVE_VER%.jar
 
-scala -classpath %$CLILIB% scala.scalanative.cli.ScalaNativeCli %* %NATIVELIB%
+set CLILIB=%BASE%\scala-native-cli-assembly_%SCALA_BIN_VER%-%SCALANATIVE_VER%.jar
+set PLUGIN=%BASE%\nscplugin_%SCALA_VER%-%SCALANATIVE_VER%.jar
+set NATIVELIB=%BASE%\nativelib%SUFFIX%;%BASE%\clib%SUFFIX%;%BASE%\posixlib%SUFFIX%;%BASE%\windowslib%SUFFIX%;%BASE%\auxlib%SUFFIX%;%BASE%\javalib%SUFFIX%;%BASE%\scalalib%SUFFIX%
+
+scala -classpath %CLILIB% scala.scalanative.cli.ScalaNativeCli  %* %NATIVELIB%
