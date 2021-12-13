56731acd2be8b83b0f28387b429cc71813ad5424
Wojciech Mazur <wojciech.mazur95@gmail.com>
2021-12-14 00:42:04 +0100

    Add bash/batch scripts

---
+++ cli/src/script/scala-native-p.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+SetLocal 
+
+set SCALA_BIN_VER=@SCALA_BIN_VER@
+set SCALANATIVE_VER=@SCALANATIVE_VER@
+
+set BASE=%~dp0\..\lib
+set CLILIB=%BASE%\scala-native-cli-assembly_%SCALA_BIN_VER%-%SCALANATIVE_VER%.jar
+
+scala -classpath %CLILIB% scala.scalanative.cli.ScalaNativeP  %*
