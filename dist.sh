e63d6914994c4b94645cba7cb3ce015766b8792b
Wojciech Mazur <wojciech.mazur95@gmail.com>
2022-09-01 21:20:04 +0200

    Release 0.4.6 and 0.4.7 (#22)
    * Release 0.4.6
    
    * Update CI runners
    
    * Update used versions of Scala in  the CI
    
    * Set jvm target flag
    
    * Set version to 0.4.7

---
+++ dist.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+
+Version=0.4.5
+Scala3Version=3.1.3
+ReleaseDir="${PWD}/release"
+TargetDir="${PWD}/cli/target"
+
+mkdir -p ${ReleaseDir}
+cd $ReleaseDir
+
+sbt clean;+cliPack
+
+for ScalaBinVer in 2.12 2.13 3; do
+  ScalaVersion=$ScalaBinVer
+  if [ "$ScalaBinVer" = "3" ]; then
+    ScalaVersion="${Scala3Version}"
+  fi
+  cd "${TargetDir}/scala-${ScalaVersion}/pack"
+
+  dirName="scala-native-cli_${ScalaBinVer}-${Version}"
+  tarName="$dirName.tgz"
+  zipName="$dirName.zip"
+  tar cfz $tarName $dirName
+  zip -r $zipName -r $dirName/
+  mv $tarName $zipName $ReleaseDir
+done
