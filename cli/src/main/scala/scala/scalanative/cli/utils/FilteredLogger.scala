f35a3b750181b3b89e2e8bb186e4f26d309dbc75
Alexandre Archambault <alexarchambault@users.noreply.github.com>
2023-01-28 16:39:45 +0100

    Send logging to stderr (#25)
    As scala-native-cli may be launched as a sub-process by other tools,
    what it prints to stdout might pollute what the other tool is sending
    to stdout. Better send its log messages to stderr straightaway.

--- cli/src/main/scala/scala/scalanative/cli/utils/FilteredLogger.scala
+++ cli/src/main/scala/scala/scalanative/cli/utils/FilteredLogger.scala
@@ -1,12 +1,22 @@
 package scala.scalanative.cli.utils
 
+import java.lang.System.err
+
 import scala.scalanative.build.Logger
 
 class FilteredLogger(
     private val verbosity: Int
 ) extends Logger {
 
-  private val underlying = Logger.default
+  private val underlying: Logger =
+    // Like Logger.default, but sending everything to stderr
+    new Logger {
+      def trace(msg: Throwable): Unit = err.println(s"[trace] $msg")
+      def debug(msg: String): Unit = err.println(s"[debug] $msg")
+      def info(msg: String): Unit = err.println(s"[info] $msg")
+      def warn(msg: String): Unit = err.println(s"[warn] $msg")
+      def error(msg: String): Unit = err.println(s"[error] $msg")
+    }
 
   override def trace(msg: Throwable): Unit = underlying.trace(msg)
   override def debug(msg: String): Unit =
