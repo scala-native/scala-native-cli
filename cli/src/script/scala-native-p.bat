23f2ebc2de1f69437209ba9c5e3688f90cdaeaa0
Wojciech Mazur <wmazur@virtuslab.com>
2024-03-13 11:50:13 +0100

    Revert windows changes to scripts, don't test standalone scripts on Windows

--- cli/src/script/scala-native-p.bat
+++ cli/src/script/scala-native-p.bat
@@ -5,6 +5,6 @@ set SCALA_BIN_VER=@SCALA_BIN_VER@
 set SCALANATIVE_VER=@SCALANATIVE_VER@
 
 set BASE=%~dp0\..\lib
-set CLILIB=%BASE%\scala-native-cli-assembly_%SCALA_BIN_VER%-%SCALANATIVE_VER%.jar
+set CLILIB="%BASE%\scala-native-cli-assembly_%SCALA_BIN_VER%-%SCALANATIVE_VER%.jar"
 
-scala -classpath "%CLILIB%" scala.scalanative.cli.ScalaNativeP %*
+scala -classpath %CLILIB% scala.scalanative.cli.ScalaNativeP  %*
